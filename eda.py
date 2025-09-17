eb7a41da5e919053102c0c3cd23793e9d490dccb
Ammar Marvi <ammar.marvi01@gmail.com>
2025-09-17 19:00:09 -0400

    loading data into python

--- eda.py
+++ eda.py
@@ -4,7 +4,14 @@ import h5py
 import numpy as np
 
 datadir = '../datasets/NNN/'
-
-
 fnames = [f for f in os.listdir(datadir) if re.match(r'^GoodUnit.*\.mat$', f)]
-print(fnames)
+
+session_num = 1
+
+f = h5py.File(os.path.join(datadir, fnames[session_num]), 'r')
+print(f['GoodUnitStrc'].keys())
+f2 = f.get('GoodUnitStrc/Raster')
+
+# the shape is correct but it is an h5py dataset (full of obj references??)
+# how do i turn it into an numpy array?
+print(f2.shape) 
