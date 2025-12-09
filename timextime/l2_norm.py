e35de421d30f28f232940d0742fcb561e5cbc1ee
Ammar Marvi <ammar.marvi01@gmail.com>
2025-12-09 10:47:37 -0500

    l2 script should work now

--- timextime/l2_norm.py
+++ timextime/l2_norm.py
@@ -12,7 +12,7 @@ BASE = slice(ONSET + BASE[0], ONSET + BASE[1])
 
 DATA_DIR = '../../datasets/NNN/'
 CATEGORY = 'face'
-STEP = 1 # choose 1 so you can change it later on
+STEP = 1 # choose 1 so you can change it later on. see L2_STEP later on
 
 # load in ROI data for a single category
 # reduces wait time since full dataset is so large
@@ -31,8 +31,36 @@ for _roi in ROI_LIST:
         cache[_roi][f'sizes_{MODE}'] = sizes
         cache[_roi][f'{MODE}_rdms'] = rdms
 
-# flatten into a dataframe
+# flatten into a dataframe to save out
 df = pd.DataFrame([
     {'ROI': roi, **vals}
     for roi, vals in cache.items()
 ])
+
+# step size of manifold scale used to calculate the L2 norm between two Time x Time RDMs
+# L2_STEP >= STEP
+L2_STEP = 5
+cols = ['ROI', 'Scale', 'Derivative', 'Mode']
+diffs = pd.DataFrame(columns=cols)
+for _roi in ROI_LIST:
+    roi_dict = cache[_roi]
+    sizes = roi_dict['sizes_top']
+   
+    for mode in ['top', 'shuff']:
+        rdms = roi_dict[f'{mode}_rdms']
+        triu = np.triu_indices_from(rdms[0], k=1)
+
+        ## single time point RDM, or average over previous time step chunk
+        # R0 = rdms[step][triu]
+        R0 = np.mean(np.array([rdm[triu] for rdm in rdms[0:step]]), axis=0) #######################################
+        for k in np.arange(1*step, len(rdms), step):
+            prev = R0
+            ## same as above
+            # R0 = rdms[t-1][triu]
+            R0 = np.mean(np.array([rdm[triu] for rdm in rdms[k:k+step]]), axis=0) ######################################
+    
+            ## difference metric for scale K
+            ## this is L2
+            diff = np.sqrt(np.sum((R0)**2))
+    
+            diffs.loc[len(diffs)] = {'ROI': _roi, 'Scale': sizes[k-1], 'Derivative': diff, 'Mode': mode}
