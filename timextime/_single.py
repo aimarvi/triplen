c3a42eed55e5dff4f7f6e57db741404df268a561
aimarvi <ammar.marvi01@gmail.com>
2026-02-06 15:37:21 -0800

    raw data plots

--- timextime/_single.py
+++ timextime/_single.py
@@ -13,15 +13,19 @@ import imageio.v2 as iio
 from _grid import *
 
 # DATA_DIR = ../../datasets/NNN/object_roi_data.pkl
-DATA_DIR = './../../datasets/NNN/object_roi_data.pkl'
+DATA_DIR = './../../datasets/NNN/face_roi_data.pkl'
 dat = pd.read_pickle(DATA_DIR)
 # ROI_LIST = ['AB3_18_B', 'MB3_12_B', 'AB3_12_B', 'AB3_17_B']
 # ROI_LIST = ['Unknown_19_F']# , 'MF1_7_F', 'MF1_8_F', 'MF1_9_F']
-ROI_LIST = ['PITP4_10_O', 'Unknown_6_O', 'MO1s2_5_O', 'Unknown_16_O',
- 'Unknown_26_O', 'AO5_25_O']
+# ROI_LIST = ['PITP4_10_O', 'Unknown_6_O', 'MO1s2_5_O', 'Unknown_16_O',
+#  'Unknown_26_O', 'AO5_25_O']
+ROI_LIST = ['Unknown_19_F']
+SAVE_DIR = './../../../buckets/manifold-dynamics/time-time/increasing-scale/'
+if not os.path.exists(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
 
 for roi in ROI_LIST:
     metric = 'correlation'
-    SAVE_PATH = f'/Users/aim/Desktop/HVRD/workspace/dynamics/gifs/{roi}_{metric}.gif'
-    # out = build_grid_gif(dat, [roi], step=1, k_max=200, metric=metric, out_path=SAVE_PATH)
+    out_path = os.path.join(SAVE_DIR, f'{roi}_{metric}.gif')
+    out = build_grid_gif(dat, [roi], step=1, k_max=300, metric=metric, out_path=out_path)
     print('Saved:', roi)
