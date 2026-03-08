20a3f3760c9e1dce2b4e935730e9c5d5c7e8da16
aimarvi <ammar.marvi01@gmail.com>
2026-03-07 21:42:42 -0500

    running all the cross val analyses

--- _run.sh
+++ _run.sh
@@ -3,29 +3,23 @@
 #SBATCH -c 1
 #SBATCH --mem=50G
 #SBATCH -t 05:00:00
-#SBATCH -o errlog/denoise.%j.out
+#SBATCH -o errlog/cv.%j.out
 
 set -euo pipefail
 
 cd /n/holylabs/LABS/konkle_lab/Users/amarvi/workspace/manifold-dynamics
-ls -la
 
-# run as a module (i think?)
+# ========== raw rasters for individual sessions
 # uv run python -m manifold_dynamics.session_raster_extraction
-
-# mutli-function via sbatch array
 # uv run python eda/single_session_raster.py roi-uid.csv single-session-raster
-uv run python denoise/single_session_gsn.py 1 denoise False 
-
-# run the script by abs. path 
+# uv run python denoise/single_session_gsn.py 1 denoise False 
 # uv run python src/manifold_dynamics/session_raster_extraction.py
 
-# cross-validation
-
-idx="${SLURM_ARRAY_TASK_ID}"
+# ========== cross-validation of 51 unique ROIs
+idx=${SLURM_ARRAY_TASK_ID}
 
 target="$(
-  uv run python -c "
+uv run python -c "
 import sys
 import pandas as pd
 import manifold_dynamics.paths as pth
@@ -37,8 +31,19 @@ uid_csv_uri = f'{pth.OTHERS}/roi-uid.csv'
 uid_csv_local = vst.fetch(uid_csv_uri)
 df = pd.read_csv(uid_csv_local)
 
-val = df.iloc[idx, 'uid']
-print(str(val))
+col = 'uid' if 'uid' in df.columns else df.columns[0]
+
+vals = (
+    df[col].astype(str)
+    .str.split('.', n=1).str[1]   # drop first portion before first '.'
+    .dropna()
+    .unique()
+)
+
+if idx < 0 or idx >= len(vals):
+    raise SystemExit(f'idx {idx} out of range (0..{len(vals)-1})')
+
+print(vals[idx])
 " "$idx"
 )"
 
@@ -47,4 +52,4 @@ if [[ -z "${target}" ]]; then
   exit 1
 fi
 
-uv run python crossval.py --target "$target" --save
+uv run python crossval.py --target "$target" --save --verbose
