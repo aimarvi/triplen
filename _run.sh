80f252b7a19c7477fa1812385d1cd5420a25e492
aimarvi <ammar.marvi01@gmail.com>
2026-03-07 19:44:09 -0500

    sbatch call for cv analysis

--- _run.sh
+++ _run.sh
@@ -19,3 +19,32 @@ uv run python denoise/single_session_gsn.py 1 denoise False
 
 # run the script by abs. path 
 # uv run python src/manifold_dynamics/session_raster_extraction.py
+
+# cross-validation
+
+idx="${SLURM_ARRAY_TASK_ID}"
+
+target="$(
+  uv run python -c "
+import sys
+import pandas as pd
+import manifold_dynamics.paths as pth
+import visionlab_utils.storage as vst
+
+idx = int(sys.argv[1])
+
+uid_csv_uri = f'{pth.OTHERS}/roi-uid.csv'
+uid_csv_local = vst.fetch(uid_csv_uri)
+df = pd.read_csv(uid_csv_local)
+
+val = df.iloc[idx, 'uid']
+print(str(val))
+" "$idx"
+)"
+
+if [[ -z "${target}" ]]; then
+  echo "no target for idx=${idx}" >&2
+  exit 1
+fi
+
+uv run python crossval.py --target "$target"
